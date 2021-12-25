5f399225212fdcd56a6813a5837ff739ba1e4877
mojiez <937691466@qq.com>
2021-12-25 22:40:56 +0800

    FinalTest

---
+++ src/main/java/com/inxianyu/xianyufordesignmodels/UserInXianyu.java
@@ -0,0 +1,15 @@
+package com.inxianyu.xianyufordesignmodels;
+
+import java.util.Scanner;
+
+public class UserInXianyu {
+    private Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+
+    public void showWhatCanUserDo(){
+        System.out.println("请输入你要选择的功能:");
+
+    }
+}
